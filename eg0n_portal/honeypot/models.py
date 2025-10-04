8f1a97c8fdd6b91433b7eb698b3b3620eeefe058
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-10-05 00:24:55 +0200

    add telnet model

--- eg0n_portal/honeypot/models.py
+++ eg0n_portal/honeypot/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # http honeypot
 class http_log(models.Model):
+    class Meta:
+        verbose_name = "01 :: HTTP Honeypot"
+        verbose_name_plural = "01 :: HTTP Honeypot"
     req_type = models.CharField(max_length=32, unique=False)        # GET, POST, HEAD
     req_path = models.CharField(max_length=256, unique=False)       # path requested
     req_header = models.TextField(blank=True, default='none')       # full header
@@ -13,3 +16,17 @@ class http_log(models.Model):
     author = models.CharField(max_length=32, editable=False, default=None)
     lastchange_author = models.CharField(max_length=32, editable=False, default=None)
 
+# telnet honeypot
+class telnet_log(models.Model):
+    class Meta:
+        verbose_name = "02 :: Telnet Honeypot"
+        verbose_name_plural = "02 :: Telnet Honeypot"
+    req_ip = models.GenericIPAddressField(unpack_ipv4=True)         # IP address
+    req_port = models.IntegerField()                                # Port
+    req_username = models.CharField(max_length=64, unique=False)    # username
+    req_password = models.CharField(max_length=64, unique=False)    # password
+    req_command = models.TextField(blank=True, default='none')      # command executed
+    log_date = models.DateField(auto_now=False, auto_now_add=True)
+    slug = models.SlugField()
+    author = models.CharField(max_length=32, editable=False, default=None)
+    lastchange_author = models.CharField(max_length=32, editable=False, default=None)   
