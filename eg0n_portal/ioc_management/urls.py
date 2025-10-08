237ea18e7bdbaa33828a6b31cea6a1c659abe145
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-10-09 00:39:56 +0200

    test frontend

--- eg0n_portal/ioc_management/urls.py
+++ eg0n_portal/ioc_management/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, include
 from . import views as ioc_views
-from django.views.generic import ListView, DetailView       # useful for template rendering
+from django.views.generic import ListView, DetailView # useful for template rendering
+from .models import IpAdd
 
 urlpatterns = [
 
     # Home page
     path('', ioc_views.home, name='Home'),
+
     # IP list
-    path('ip_list/', ioc_views.ip_list, name='IP Address List'),
+    # path('ip_list/', ioc_views.ip_list, name='IP Address List'),
+    path('ip_list/', ListView.as_view(
+        queryset = IpAdd.objects.all().order_by('-publish_date'),
+        tamplte_name = 'ip_list.html'), name='IP Address List'),
+
     # Domain list
     path('domain_list/', ioc_views.domain_list, name='DomainName List'),
 
--- eg0n_portal/ioc_management/views.py
+++ eg0n_portal/ioc_management/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+'''
 def ip_list(request):
     return render(request, 'ip_list.html')
+'''
 
 def domain_list(request):
     return render(request, 'domain_list.html')
