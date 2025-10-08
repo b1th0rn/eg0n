aa28f6500be5e1d23c6525edd63e4a6e4de17dfb
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-10-09 00:16:41 +0200

    add template home

--- eg0n_portal/eg0n_portal/urls.py
+++ eg0n_portal/eg0n_portal/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include("ioc_management.urls"))
+    path('', include("ioc_management.urls")),
+    path('ioc_view/', include("ioc_management.urls"))
 ]
--- eg0n_portal/ioc_management/urls.py
+++ eg0n_portal/ioc_management/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from . import views as ioc_views
 from django.views.generic import ListView, DetailView       # useful for template rendering
-from .models import Vuln
 
 urlpatterns = [
 
-    path('', ioc_views.home, name="Home"),
+    # Home page
+    path('', ioc_views.home, name='Home'),
+    # IP list
+    path('ip_list/', ioc_views.ip_list, name='IP Address List'),
+    # Domain list
+    path('domain_list/', ioc_views.domain_list, name='DomainName List'),
 
-    # path('vulns', vulns_views.vulns_list, name="Vulns"),  # init test
-    path('vulns', ListView.as_view(queryset = Vuln.objects.all().order_by("update_date"), template_name = "vulns_list.html"), name="Vulns"),
-    
-    path('vulns-details', ioc_views.vulns_details, name="Vulns Details"),
 ]
