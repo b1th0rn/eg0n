01e7bbd0743415c361daa3f217d5412c0f30c8f2
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-10-08 23:25:50 +0200

    fix

--- eg0n_portal/infosec_dashboard/models.py
+++ eg0n_portal/infosec_dashboard/models.py
@@ -1,31 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
-from info_gathering.models import OrganizationInfo, PersonInfo
-from ioc_management.models import Vuln
 from django.utils import timezone
-
-# Asset Models
-class Asset(models.Model):
-    NETWORK_ZONE_LIST = [ ('client', 'client'), ('server', 'server'), ('dmz', 'dmz'), ('public', 'public') ]
-    IMPACT_LEVEL = [ ('low', 'low'), ('medium', 'medium'), ('high', 'high') ]
-    YES_NO = [ ('yes', 'yes'), ('no', 'no') ]
-    organization = models.ForeignKey(OrganizationInfo, to_field="organization_name", on_delete=models.CASCADE, db_column="organization_name")
-    owner = models.ForeignKey(PersonInfo, to_field="name", on_delete=models.CASCADE, db_column="name")
-    hostname = models.CharField(max_length=32, unique=True)
-    ipadd = models.CharField(max_length=32)
-    network_zone = models.CharField(max_length=16, choices=NETWORK_ZONE_LIST)
-    behind_utm_firewall = models.CharField(max_length=16, choices=YES_NO)
-    edr_active = models.CharField(max_length=16, choices=YES_NO)
-    soc_scope = models.CharField(max_length=16, choices=YES_NO)
-    sensitive_data = models.CharField(max_length=16, choices=YES_NO)
-    business_impact = models.CharField(max_length=16, choices=IMPACT_LEVEL)
-    end_of_life = models.DateField(default=timezone.now)
-    description = models.TextField()
-    
-    def __str__(self):
-        return self.hostname
-
-class CriticalVuln(models.Model):
-    asset = models.ForeignKey(Asset, to_field="hostname", on_delete=models.CASCADE, db_column="hostname")
-    cve_critical = models.OneToOneField(Vuln, to_field="cve", on_delete=models.CASCADE, db_column="cve", related_name="cve_critical", default="none")
-    owner = models.ForeignKey(PersonInfo, to_field="name", on_delete=models.CASCADE, db_column="name")
