49ac431bff51946edea0b49a68d6fdd09a4ae56c
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-10-10 21:05:10 +0200

    fix urls core

--- eg0n_portal/eg0n_portal/urls.py
+++ eg0n_portal/eg0n_portal/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("ioc_management.urls")),
-    path('ioc_view/', include("ioc_management.urls"))
+    path('', include("core.urls")),
+    path('ioc_view/', include("core.urls"))
 ]
