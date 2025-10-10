6021fde7c984abdae5f99bf55b7590e42e7821c4
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-10-10 10:34:48 +0200

    add baseConfig model

--- eg0n_portal/core/admin.py
+++ eg0n_portal/core/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import apiConfig
+from .models import apiConfig, BaseConfig
+
+class BaseConfigAdmin(admin.ModelAdmin):
+    list_display = ["param_name", "param_value", "param_description"]
+    search_fields = ["param_name", "param_value", "param_description"]
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user.username
+        obj.lastchange_author = request.user.username
+        return super().save_model(request, obj, form, change)
+    class Meta:
+        model = BaseConfig
 
 class apiConfigAdmin(admin.ModelAdmin):
     list_display = ["api_name", "api_url", "api_description"]
     search_fields = ["api_name", "api_url"]
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user.username
+        obj.lastchange_author = request.user.username
+        return super().save_model(request, obj, form, change)
     class Meta:
         model = apiConfig
 
+admin.site.register(BaseConfig, BaseConfigAdmin)
 admin.site.register(apiConfig, apiConfigAdmin)
--- eg0n_portal/core/models.py
+++ eg0n_portal/core/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
+# base configuration model
+class BaseConfig(models.Model):
+    class Meta:
+        verbose_name = "00 :: Base Configuration"
+        verbose_name_plural = "00 :: Base Configurations"
+    param_name = models.CharField(max_length=32, unique=True)
+    param_value = models.CharField(max_length=128)
+    param_description = models.TextField()
+    create_date = models.DateField(auto_now=False, auto_now_add=True)
+    update_date = models.DateField(auto_now=True, auto_now_add=False)
+    author = models.CharField(max_length=32, editable=False, default=None)
+    lastchange_author = models.CharField(max_length=32, editable=False, default=None)
+    def __str__(self):
+        return self.param_name
+
 # base api configuration
 class apiConfig(models.Model):
     class Meta:
-        verbose_name = "00 :: API Configuration"
-        verbose_name_plural = "00 :: API Configurations"
+        verbose_name = "01 :: API Configuration"
+        verbose_name_plural = "01 :: API Configurations"
     api_name = models.CharField(max_length=32, unique=True)
     api_url = models.CharField(max_length=128)
     api_key = models.CharField(max_length=128)
