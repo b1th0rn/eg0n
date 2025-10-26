9a2f2345d6fc045fd0520af84a665138661c5614
Andrea Dainese <andrea.dainese@pm.me>
2025-10-26 09:45:46 +0100

    update

---
+++ eg0n_portal/tests/ui/test_ui_profile_view_api.py
@@ -0,0 +1,16 @@
+"""Test API profile view."""
+
+import pytest
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+
+
+@pytest.mark.parametrize("role", ["admin", "staff", "user"])
+def test_ui_profile_view_api(api_client, user_sets, role):
+    """Test API get profile."""
+    user = user_sets[0][role]
+    token, _ = Token.objects.get_or_create(user=user)
+    headers = {"Authorization": f"Token {token}"}
+    url = reverse("user-detail", kwargs={"pk": user.id})
+    response = api_client.get(url, headers=headers)
+    assert response.status_code == 200
