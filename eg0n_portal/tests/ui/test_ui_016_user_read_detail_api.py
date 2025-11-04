4b29c29b33036a26e933e205f0527bd427a08173
Andrea Dainese <andrea.dainese@pm.me>
2025-11-04 16:15:57 +0100

    update

--- eg0n_portal/tests/ui/test_ui_016_user_read_detail_api.py
+++ eg0n_portal/tests/ui/test_ui_016_user_read_detail_api.py
@@ -1,4 +1,4 @@
-"""Test API user list view."""
+"""Test API user detail view."""
 
 import pytest
 from django.contrib.auth.models import User
@@ -7,24 +7,23 @@ from rest_framework.authtoken.models import Token
 
 
 @pytest.mark.django_db
-def test_ui_user_read_list_api_admin(
+def test_ui_user_read_detail_api_admin(
     api_client,
     user_set_group1,
     user_set_group_multiple,
     user_set_ungrouped,
     user_set_single,
 ):
-    """Test DRF (API) user list view by admin."""
+    """Test DRF (API) user detail view by admin."""
     for user in User.objects.filter(is_superuser=True):
         token, _ = Token.objects.get_or_create(user=user)
         headers = {"Authorization": f"Token {token}"}
-        all_users = list(User.objects.all().values_list("username", flat=True))
-        url = reverse("user-list") + f"?per_page={len(all_users)}"
-        response = api_client.get(url, headers=headers)
-        assert response.status_code == 200, f"Failed for user {user.username}"
-        result_users = [u["username"] for u in response.data["results"]]
-        for u in all_users:
-            assert u in result_users, f"User {u} not found for {user.username}"
+        for u in User.objects.all():
+            url = reverse("user-detail", kwargs={"pk": u.id})
+            response = api_client.get(url, headers=headers)
+            assert (
+                response.status_code == 200
+            ), f"User {u.username} not found for {user.username}"
 
 
 @pytest.mark.django_db
@@ -35,7 +34,7 @@ def test_ui_user_read_list_api_user(
     user_set_ungrouped,
     user_set_single,
 ):
-    """Test DRF (API) user list view by staffs and users."""
+    """Test DRF (API) user detail view by staffs and users."""
     for user in User.objects.filter(is_superuser=False):
         token, _ = Token.objects.get_or_create(user=user)
         headers = {"Authorization": f"Token {token}"}
@@ -45,17 +44,20 @@ def test_ui_user_read_list_api_user(
         assert response.status_code == 200, f"Failed for user {user.username}"
         result_users = [u["username"] for u in response.data["results"]]
         # Testing users with a shared group
-        all_users_w_shared_group = list(
-            all_users.filter(groups__in=user.groups.all())
-            .distinct()
-            .values_list("username", flat=True)
-        )
+        all_users_w_shared_group = all_users.filter(groups__in=user.groups.all())
         for u in all_users_w_shared_group:
-            assert u in result_users, f"User {u} not found for {user.username}"
+            url = reverse("user-detail", kwargs={"pk": u.id})
+            response = api_client.get(url, headers=headers)
+            assert (
+                response.status_code == 200
+            ), f"User {u.username} not found for {user.username}"
         # Testing users without a shared group
         all_users_wo_shared_group = list(
             all_users.exclude(
-                username__in=all_users_w_shared_group + [user.username]
+                username__in=list(
+                    all_users_w_shared_group.values_list("username", flat=True)
+                )
+                + [user.username]
             ).values_list("username", flat=True)
         )
         for u in all_users_wo_shared_group:
@@ -63,8 +65,9 @@ def test_ui_user_read_list_api_user(
 
 
 @pytest.mark.django_db
-def test_ui_user_read_list_api_guest(api_client):
-    """Test DRS (API) user list view by guest user."""
-    url = reverse("user-list")
-    response = api_client.get(url)
-    assert response.status_code == 401, "Expected 401 for guest user"
+def test_ui_user_read_list_api_guest(api_client, user_set_group1):
+    """Test DRS (API) user detail view by guest user."""
+    for u in User.objects.all():
+        url = reverse("user-detail", kwargs={"pk": u.id})
+        response = api_client.get(url)
+        assert response.status_code == 401, "Expected 401 for guest user"
